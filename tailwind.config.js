a42acc40b77ceb508f6d4cca616b85350fcbec92
Mikey <ntquan612@gmail.com>
2022-04-11 21:30:00 +0700

    flight booking

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	purge: ["./**/*.html"],
+	mode: "jit",
+	darkMode: "class",
+	content: [],
+	theme: {
+		extend: {
+			colors: {
+				blue3B7: "#3B71FE",
+				c9: "#FFFFFF",
+				c8: "#FCFCFD",
+				c7: "#F4F5F6",
+				c6: "#E7ECF3",
+				c5: "#B1B5C3",
+				c4: "#84878B",
+				c3: "#3B3E44",
+				c2: "#222529",
+				c1: "#18191B",
+			},
+		},
+	},
+	plugins: [],
+};
